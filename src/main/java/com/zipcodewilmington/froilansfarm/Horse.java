9e2b5be17120e2425393e830b16e161a2efed944
terrik <terrik_smith@yahoo.com>
2026-08-28 11:33:27 -0400

    Implement horse rideable behavior tested and passed

--- src/main/java/com/zipcodewilmington/froilansfarm/Horse.java
+++ src/main/java/com/zipcodewilmington/froilansfarm/Horse.java
@@ -1,4 +1,25 @@
 package com.zipcodewilmington.froilansfarm;
 
-public class Horse {
+public class Horse extends Animal implements Rideable {
+
+    private boolean mounted;
+
+    @Override
+    public String makeNoise() {
+        return "Neigh";
+    }
+
+    @Override
+    public void beMounted(Rider rider) {
+        mounted = true;
+    }
+
+    @Override
+    public void beDismounted(Rider rider) {
+        mounted = false;
+    }
+
+    public boolean isMounted() {
+        return mounted;
+    }
 }
--- src/main/java/com/zipcodewilmington/froilansfarm/Rideable.java
+++ src/main/java/com/zipcodewilmington/froilansfarm/Rideable.java
@@ -1,4 +1,8 @@
 package com.zipcodewilmington.froilansfarm;
 
 public interface Rideable {
+
+    void beMounted(Rider rider);
+
+    void beDismounted(Rider rider);
 }
