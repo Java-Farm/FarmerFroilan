d922c2fb9df6d9133bc1ad125b26c2a6a3aa369e
mecca <mecca.benson@gmail.com>
2026-08-28 00:37:03 -0400

    test: implement chicken behavior

--- src/main/java/com/zipcodewilmington/froilansfarm/Chicken.java
+++ src/main/java/com/zipcodewilmington/froilansfarm/Chicken.java
@@ -1,4 +1,26 @@
 package com.zipcodewilmington.froilansfarm;
 
 public class Chicken {
+
+    private boolean fertilized;
+
+    public Chicken() {
+        this(false);
+    }
+
+    public Chicken(boolean fertilized) {
+        this.fertilized = fertilized;
+    }
+
+    public String makeNoise() {
+        return "Cluck";
+    }
+
+    public Edible yield() {
+        if (fertilized) {
+            return null;
+        }
+
+        return new EdibleEgg();
+    }
 }
