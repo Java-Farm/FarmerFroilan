ae58db19500dd04c33699b6b9c87550e04cceb5c
mecca <mecca.benson@gmail.com>
2026-08-28 15:43:44 -0400

    feat: implement crop row and field storage

--- src/test/java/com/zipcodewilmington/froilansfarm/CropRowTest.java
+++ src/test/java/com/zipcodewilmington/froilansfarm/CropRowTest.java
@@ -1,4 +1,17 @@
 package com.zipcodewilmington.froilansfarm;
 
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 class CropRowTest {
+
+    @Test
+    void cropRowShouldStoreCrop() {
+        CropRow<CornStalk> cropRow = new CropRow<>();
+
+        cropRow.addCrop(new CornStalk());
+
+        assertEquals(1, cropRow.getCrops().size());
+    }
 }
