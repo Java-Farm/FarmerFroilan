6cf08f806ae7e73090129144d18c75da58f4ccdc
mecca <mecca.benson@gmail.com>
2026-08-30 18:14:53 -0400

    feat: implement farm structure composition

--- src/test/java/com/zipcodewilmington/froilansfarm/FarmHouseTest.java
+++ src/test/java/com/zipcodewilmington/froilansfarm/FarmHouseTest.java
@@ -1,4 +1,20 @@
 package com.zipcodewilmington.froilansfarm;
 
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertSame;
+
 class FarmHouseTest {
+
+    @Test
+    void farmHouseShouldStorePeople() {
+        FarmHouse farmHouse = new FarmHouse();
+        Person person = new Froilanda();
+
+        farmHouse.add(person);
+
+        assertEquals(1, farmHouse.size());
+        assertSame(person, farmHouse.getResidents().get(0));
+    }
 }
